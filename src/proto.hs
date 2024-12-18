ba7d2f868477bd29c4bad8409ca5d4d6b81c7809
yourcomrade <MinhXecole@gmail.com>
2024-12-18 17:12:41 +0100

    Lift instance causess error unknown symbol mpfr_add during cabal build but not cabal repl

--- src/proto.hs
+++ src/proto.hs
@@ -1,80 +1,92 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
-module Proto 
-where
-    
-import FPFloat
-import qualified Data.Number.MPFR as M 
-import Clash.Explicit.Prelude as C
-import Data.String.Interpolate (__i)
-import Clash.Annotations.Primitive (Primitive(..), HDL(..))
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-import Control.Monad.State (State, liftIO)
-import qualified Data.List as L
-import Data.Text
-import Data.Text.Prettyprint.Doc.Extra (Doc)
-import GHC.Stack (HasCallStack)
-import Clash.Backend (Backend)
-import Clash.Netlist.BlackBox.Types
-  (BlackBoxFunction, BlackBoxMeta(..), TemplateKind(..), emptyBlackBoxMeta)
-import qualified Clash.Netlist.Id as Id
-import Clash.Netlist.Types
-  (BlackBox (..), BlackBoxContext, EntityOrComponent(..), TemplateFunction(..), HWType, bbResults)
-import qualified Clash.Netlist.Types as N
-import qualified Clash.Primitives.DSL as DSL
-
-import Data.Maybe (fromMaybe, isJust, fromJust)
+module Proto where
 
 import Clash.Annotations.BitRepresentation
-import System.IO.Unsafe
--- | floating point addition, assume pipeline depth of 2
-import Language.Haskell.TH.Syntax (Name)
-import Debug.Trace (trace, traceShow)
-import Clash.Primitives.Types (Primitive(BlackBoxHaskell, workInfo))
---import Clash.Driver.Bool (OverridingBool(Always))
-import Text.Show.Pretty(ppShow)
+import Clash.Annotations.Primitive (HDL (..), Primitive (..))
+import Clash.Backend (Backend)
+import Clash.Explicit.Prelude as C
+-- \| floating point addition, assume pipeline depth of 2
+
+-- import Clash.Driver.Bool (OverridingBool(Always))
+
 import Clash.Explicit.Testbench
+import Clash.Netlist.BlackBox.Types
+  ( BlackBoxFunction,
+    BlackBoxMeta (..),
+    TemplateKind (..),
+    emptyBlackBoxMeta,
+  )
+-- import qualified Clash.Netlist.Types as DSL
 
---import qualified Clash.Netlist.Types as DSL
-import qualified Data.Text as Text
 import Clash.Netlist.BlackBox.Util (bbResult)
+import qualified Clash.Netlist.Id as Id
+import Clash.Netlist.Types
+  ( BlackBox (..),
+    BlackBoxContext,
+    EntityOrComponent (..),
+    HWType,
+    TemplateFunction (..),
+    bbResults,
+  )
+import qualified Clash.Netlist.Types as N
+import qualified Clash.Primitives.DSL as DSL
+import Clash.Primitives.Types (Primitive (BlackBoxHaskell, workInfo))
+import Control.Monad.State (State, liftIO)
+import qualified Data.List as L
+import Data.Maybe (fromJust, fromMaybe, isJust)
+import qualified Data.Number.MPFR as M
 import Data.Proxy
-
+import Data.String.Interpolate (__i)
+import Data.Text
+import qualified Data.Text as Text
+import Data.Text.Prettyprint.Doc.Extra (Doc)
+import Debug.Trace (trace, traceShow)
+import FPFloat
+import GHC.Stack (HasCallStack)
+import Language.Haskell.TH.Syntax (Name)
+import System.IO.Unsafe
+import Text.Show.Pretty (ppShow)
 
 type N = 1
-xp = SNat::SNat N
 
-plusFloatBV
-  :: forall exponentBits mantissaBits rndMode n . 
+xp = SNat :: SNat N
+
+plusFloatBV ::
+  forall exponentBits mantissaBits rndMode n.
   (KnownNat exponentBits, KnownNat mantissaBits) =>
-  Proxy (FoFloat exponentBits mantissaBits rndMode)
-  -> Clock System
-  -> DSignal System n (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode)))
-  -> DSignal System n (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode)))
-  -> DSignal System (n + N) (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode)))
+  Proxy (FoFloat exponentBits mantissaBits rndMode) ->
+  Clock System ->
+  DSignal System n (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode))) ->
+  DSignal System n (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode))) ->
+  DSignal System (n + N) (BitVector (BitSize (FoFloat exponentBits mantissaBits rndMode)))
 plusFloatBV _ clk a b = delayN xp undefined enableGen clk (a + b)
 
-plusFloat
-  :: forall n . 
-  Clock System
-  -> DSignal System n (FoFloat 4 11 M.Near)
-  -> DSignal System n (FoFloat 4 11 M.Near)
-  -> DSignal System (n + N) (FoFloat 4 11 M.Near)
+plusFloat ::
+  forall n.
+  Clock System ->
+  DSignal System n (FoFloat 4 11 M.Near) ->
+  DSignal System n (FoFloat 4 11 M.Near) ->
+  DSignal System (n + N) (FoFloat 4 11 M.Near)
 plusFloat clk a b = C.unpack <$> plusFloatBV (Proxy @(FoFloat 4 11 M.Near)) clk (C.pack <$> a) (C.pack <$> b)
 
 {-# OPAQUE plusFloatBV #-}
-{-# ANN plusFloatBV (
-    let
-      primName = show 'plusFloatBV
-      tfName = show 'plusFloatBBF
-    in
-      InlineYamlPrimitive [minBound..] [__i|
+{-# ANN
+  plusFloatBV
+  ( let primName = show 'plusFloatBV
+        tfName = show 'plusFloatBBF
+     in InlineYamlPrimitive
+          [minBound ..]
+          [__i|
         BlackBoxHaskell:
           name: #{primName}
           templateFunction: #{tfName}
           workInfo: Always
-      |]) #-}
+      |]
+  )
+  #-}
 
-      
 plusFloatBBF :: BlackBoxFunction
 plusFloatBBF _isD _primName _args _resTys = do
   -- Call Flopoco
@@ -83,16 +95,16 @@ plusFloatBBF _isD _primName _args _resTys = do
 
   -- let entityName = parseEntityName output
   let entityName = "plusFloat"
-  
+
   let meta = emptyBlackBoxMeta {bbKind = TDecl}
       bb = BBFunction (show 'plusFloatTF) 0 (plusFloatTF entityName)
   -- Debugging output
-  --trace (show meta) $ return ()
-  --trace (show bb) $ return ()
+  -- trace (show meta) $ return ()
+  -- trace (show bb) $ return ()
   pure (Right (meta, bb))
 
 plusFloatTF ::
-  HasCallStack =>
+  (HasCallStack) =>
   Text ->
   TemplateFunction
 plusFloatTF entityName =
@@ -102,43 +114,49 @@ plusFloatTF entityName =
     (plusFloatBBTF entityName)
 
 plusFloatBBTF ::
-  forall s .
-  Backend s =>
+  forall s.
+  (Backend s) =>
   Text ->
   BlackBoxContext ->
   State s Doc
 plusFloatBBTF entityName bbCtx
-  | [ clk, a, b
-    ] <- L.map fst (DSL.tInputs bbCtx)
-  , [result] <- DSL.tResults bbCtx
-  = do
+  | [ clk,
+      a,
+      b
+      ] <-
+      L.map fst (DSL.tInputs bbCtx),
+    [result] <- DSL.tResults bbCtx =
+      do
+        plusFloatInstName <- Id.makeBasic "plusFloat_inst"
 
-    plusFloatInstName <- Id.makeBasic "plusFloat_inst"
+        let compInps =
+              [ ("clk", N.Bit),
+                ("X", DSL.ety a),
+                ("Y", DSL.ety b)
+              ]
+            compOuts =
+              [("R", DSL.ety result)]
 
-    let
-      compInps =
-        [ ("clk", N.Bit)
-        , ("X", DSL.ety a)
-        , ("Y", DSL.ety b) ]
-      compOuts =
-        [ ("R", DSL.ety result) ]
+        DSL.declaration "plusFloat_inst_block" $ do
+          DSL.compInBlock entityName compInps compOuts
 
-    DSL.declaration "plusFloat_inst_block" $ do
-      DSL.compInBlock entityName compInps compOuts
+          let inps =
+                [ ("clk", clk),
+                  ("X", a),
+                  ("Y", b)
+                ]
 
-      let
-        inps =
-          [ ("clk", clk )
-          , ("X", a)
-          , ("Y", b)
-          ]
+              outs =
+                [ ("R", result)
+                ]
 
-        outs =
-          [ ("R", result)
-          ]
-
-      DSL.instDecl Empty (Id.unsafeMake entityName) plusFloatInstName
-        [] inps outs
+          DSL.instDecl
+            Empty
+            (Id.unsafeMake entityName)
+            plusFloatInstName
+            []
+            inps
+            outs
   | otherwise = error $ ppShow bbCtx
 
 topEntity ::
@@ -148,21 +166,29 @@ topEntity ::
   DSignal System (0 + N) (FoFloat 4 11 M.Near)
 topEntity clk x y =
   plusFloat clk x y
-
 {-# OPAQUE topEntity #-}
-{-# ANN topEntity
-  (Synthesize
-    { t_name   = "topEntity"
-    , t_inputs = [ PortName "clk"
-                 , PortName "x" 
-                 , PortName "y"
-                 ]
-    , t_output = PortName "result"
-    }) #-}
+{-# ANN
+  topEntity
+  ( Synthesize
+      { t_name = "topEntity",
+        t_inputs =
+          [ PortName "clk",
+            PortName "x",
+            PortName "y"
+          ],
+        t_output = PortName "result"
+      }
+  )
+  #-}
 
+-- Lift instance causess error unknown symbol "mpfr_add" during cabal build but not cabal repl
+ta = $(lift (1.2 :: FoFloat 4 11 M.Near))
+
+{-
 testBench = fmap C.pack $ toSignal $ topEntity clk  (fromSignal a) (fromSignal b)
      where
          a = stimuliGenerator clk rst $(lift (1.2 :> 14 :> 23 :> (-4.3) :> (-5.6) :> Nil :: Vec 5 (FoFloat 4 11 M.Near)))
          b = stimuliGenerator clk rst $(lift (1.2 :> 1.4 :> 2.3 :> (-4.3) :> (-5.6) :> Nil :: Vec 5 (FoFloat 4 11 M.Near)))
          clk = systemClockGen
          rst = systemResetGen
+-}
